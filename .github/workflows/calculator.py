8ec99829699c3cd5e9363258f5c5df9c42f114b5
Alex5 <alexandruku@gmail.com>
2025-04-18 18:52:31 +0300

    update

---
+++ .github/workflows/calculator.py
@@ -0,0 +1,25 @@
+def add(x, y):
+    """Функція додавання."""
+    return x + y
+
+def subtract(x, y):
+    """Функція віднімання."""
+    return x - y
+
+def multiply(x, y):
+    """Функція множення."""
+    return x * y
+
+def divide(x, y):
+    """Функція ділення."""
+    if y == 0:
+        raise ZeroDivisionError("Ділення на нуль неможливе")
+    return x / y
+
+def multiply_list(listm):
+"""Функція множення елементів списку."""
+result = 1
+for item in listm:
+result *= item
+return result
+
